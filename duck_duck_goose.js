45ee17e0d9ee42862c45f34585dc0ac21ee633f5
Muhammad Aufa <aufalfirdausi@gmail.com>
2025-07-18 20:15:03 +0700

    game duck_duck_goose

---
+++ duck_duck_goose.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 10 )
+
+while (randomNumber != 8) {
+    console.log("Duck")
+    randomNumber = Math.floor(Math.random () * 10)
+}
+
+console.log("Goose")
